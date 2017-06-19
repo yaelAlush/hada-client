647634c72971378c7a4a8a4b7a9285713f009473
yaelAlush <alush.yael@gmail.com>
2017-06-19 16:06:12 +0300

    add popup

--- HadaClient/src/app/app.module.ts
+++ HadaClient/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { DishDetailsPopupComponent } from './dish-details-popup/dish-details-popup';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    DishDetailsPopupComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +25,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    DishDetailsPopupComponent
   ],
   providers: [
     StatusBar,
--- HadaClient/src/pages/home/home.ts
+++ HadaClient/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, PopoverController } from 'ionic-angular';
+import { DishDetailsPopupComponent } from "../../app/dish-details-popup/dish-details-popup";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    public popoverCtrl: PopoverController) {
 
   }
+
+  presentPopover(myEvent) {
+    let popover = this.popoverCtrl.create(DishDetailsPopupComponent,{hello:"sss"});
+    popover.present({
+      ev: myEvent
+    });
+  }
+
+
 
 }
