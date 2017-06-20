5870989f707ab50e2948b98c07b1d4fdc9036063
yohav <yohav3@gmail.com>
2017-06-20 11:35:06 +0300

    add props

--- HadaClient/src/pages/prizes/prizes.ts
+++ HadaClient/src/pages/prizes/prizes.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from "ionic-angular";
 import {PointsService} from "../../app/points.service";
 import {PrizesService} from "../../app/prizes.service";
 import {Prize} from "../../app/models/prize.model";
@@ -10,7 +11,7 @@ import {Prize} from "../../app/models/prize.model";
 export class PrizesPage implements OnInit {
   public points;
   public prizes: Prize[];
-  constructor(private pointsService: PointsService, private prizesService: PrizesService){
+  constructor(private pointsService: PointsService, private prizesService: PrizesService, public toastCtrl: ToastController){
     this.points = pointsService.getPoints();
     this.prizesService.getPrizes().then(prizes => this.prizes = prizes);
   }
@@ -21,10 +22,23 @@ export class PrizesPage implements OnInit {
   getPrize(prize){
     this.pointsService.subPoints(prize.points);
     this.points-= prize.points;
+    this.presentToast(prize)
   }
 
   isDisabled(prize){
     return prize.points > this.points;
+  }
+
+  presentToast(prize) {
+    var message;
+    message= "מזל טוב! זכית ב" + prize.name;
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 2000,
+      cssClass: 'points-toast'
+
+    });
+    toast.present();
   }
 
 }
